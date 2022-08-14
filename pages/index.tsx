55035ecd4dcd5d04fbb6ad9fac965687447dd879
Harsh Singh <hi.harsh@protonmail.ch>
2022-08-14 15:05:31 -0500

    Update language names and options

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import Editor from '@components/Editor'
 import Wrapper from '@components/Wrapper'
 import type { NextPage } from 'next'
 import { useState } from 'react'
+import Palette from '@components/Palette/Main'
+import langs from '@lib/languages'
 
 const Home: NextPage = () => {
+  const [language, setLanguage] = useState<keyof typeof langs>('javascript')
+
   return (
     <Wrapper>
-      <Editor language='javascript' />
+      <Palette language={language} setLanguage={setLanguage} />
+      <Editor language={language} />
     </Wrapper>
   )
 }
