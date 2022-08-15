d18fc3db4e7b1817523b1ec710ead45547d37045
ibra <muhammadibrahimhisham@gmail.com>
2022-08-15 20:56:17 +0500

    add dotenv and postgrest

--- api/users/snips.rs
+++ api/users/snips.rs
@@ -1,8 +1,16 @@
+extern crate dotenv;
+use dotenv::dotenv;
+use std::env;
+
 use http::StatusCode;
+use postgrest::Postgrest;
 use std::error::Error;
 use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
 fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
+    let client = Postgrest::new("https://araasnleificjyjflqml.supabase.co/rest")
+        .insert_header("apikey", &env::var("SUPABASE_API_KEY").unwrap());
+
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/plain")
@@ -12,7 +20,7 @@ fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
     Ok(response)
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
     Ok(lambda!(handler))
 }
