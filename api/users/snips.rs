7158df24d41f816ec9bc333b6da9ce68a2660935
ibra <muhammadibrahimhisham@gmail.com>
2022-08-15 21:18:35 +0500

    remove postgrest because vercels rust serverless functions are fucking dumb

--- api/users/snips.rs
+++ api/users/snips.rs
@@ -1,16 +1,11 @@
 extern crate dotenv;
 use dotenv::dotenv;
-use std::env;
 
 use http::StatusCode;
-use postgrest::Postgrest;
 use std::error::Error;
 use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
 fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
-    let client = Postgrest::new("https://araasnleificjyjflqml.supabase.co/rest")
-        .insert_header("apikey", &env::var("SUPABASE_API_KEY").unwrap());
-
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/plain")
