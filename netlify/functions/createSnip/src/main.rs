98df86dc0c807706cd4dd3ca9ce1f197d92db34d
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2022-08-19 01:00:42 +0500

    made create function with dummy data

--- netlify/functions/createSnip/src/main.rs
+++ netlify/functions/createSnip/src/main.rs
@@ -1,38 +1,29 @@
-use aws_lambda_events::encodings::Body;
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
 use http::header::HeaderMap;
 use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
 use postgrest::Postgrest;
 use dotenv::dotenv;
 use std::env;
 
-use log::LevelFilter;
-use simple_logger::SimpleLogger;
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    SimpleLogger::new()
-        .with_level(LevelFilter::Info)
-        .init()
-        .unwrap();
-    dotenv().ok();
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
 
     let func = handler_fn(my_handler);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-pub(crate) async fn my_handler(
-    event: ApiGatewayProxyRequest,
-    _ctx: Context,
-) -> Result<ApiGatewayProxyResponse, Error> {
-
+pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
     let client = Postgrest::new("https://araasnleificjyjflqml.supabase.co/rest/v1/")
     .insert_header("apikey", &env::var("SUPABASE_PUBLIC_ANON_KEY").unwrap());
 
-    let resp = client.from("snips")
-    .select("*")
-    .eq("id", event.query_string_parameters.first("id").unwrap())
+    let resp = client
+    .from("snips")
+    .insert(r#"[{"id":"blabla","code":"println!(\"hello world!\");"}]"#)
     .execute().await?;
 
     let body = resp.text().await?;
@@ -44,6 +35,5 @@ pub(crate) async fn my_handler(
         body: Some(Body::Text(body)),
         is_base64_encoded: Some(false),
     };
-
     Ok(resp)
 }
