98a6625225c2931ecf51cf7077ac5d199ccc2c49
Harsh Singh <hi.harsh@protonmail.ch>
2022-08-20 03:47:53 -0500

    Added database init file

---
+++ initdb.sql
@@ -0,0 +1,21 @@
+CREATE TYPE expires AS ENUM (
+    'never',
+    '1h',
+    '2h',
+    '10h',
+    '1d',
+    '2d',
+    '1w',
+    '1m',
+    '1y'
+);
+CREATE TABLE snips (
+    id varchar(255) PRIMARY KEY,
+    code TEXT NOT NULL,
+    password TEXT,
+    language TEXT,
+    expires_in expires,
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id UUID,
+    CONSTRAINT user FOREIGN KEY(user_id) REFERENCES user(id)
+)
