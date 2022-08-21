a63ace9989e18e79d854a97a343e58142324e05f
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2022-08-21 05:36:36 +0500

    add paste creation with id, code and language

--- api/snip_new/src/main.rs
+++ api/snip_new/src/main.rs
@@ -23,6 +23,11 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
+// struct Paste {
+//     paste_id: String,
+//     code: &str,
+// }
+
 pub(crate) async fn my_handler(
     event: ApiGatewayProxyRequest,
     _ctx: Context,
@@ -30,8 +35,11 @@ pub(crate) async fn my_handler(
     let mut response_headers: HeaderMap = HeaderMap::new();
     response_headers.insert("Content-Type", "application/json".parse().unwrap());
 
-    let paste_id = nanoid!(10);
     let request_body: Value;
+
+    let mut paste_id = nanoid!(10);
+    let language: String;
+    let code: &str;
 
     let client = Postgrest::new("https://araasnleificjyjflqml.supabase.co/rest/v1/")
         .insert_header("apikey", &env::var("SUPABASE_PUBLIC_ANON_KEY").unwrap());
@@ -53,10 +61,39 @@ pub(crate) async fn my_handler(
         }
     }
 
+    if !request_body["id"].is_null() {
+        match request_body["id"].as_str() {
+            Some(value) => {
+                paste_id = value.to_owned();
+            }
+            None => todo!("id does not contain value"),
+        }
+    }
+    if let Some(value) = request_body["code"].as_str() {
+        code = value;
+    } else {
+        let resp = ApiGatewayProxyResponse {
+            status_code: 400,
+            headers: response_headers,
+            multi_value_headers: HeaderMap::new(),
+            body: Some(Body::Text(
+                r#"{ "statusCode": 400, "message": "Missing required key in body [code]!" }"#
+                    .to_owned(),
+            )),
+            is_base64_encoded: Some(false),
+        };
+        return Ok(resp);
+    }
+
+    match request_body["language"].as_str() {
+        Some(value) => language = format!("\"{value}\""),
+        None => language = "null".to_owned(),
+    }
+
     let resp = client
         .from("snips")
         .insert(format!(
-            r#"[ {{ "id": "{paste_id}", "code": "println!(\"hello world!\");"}} ]"#,
+            r#"[ {{ "id": "{paste_id}", "code": "{code}", "language": {language} }} ]"#
         ))
         .execute()
         .await?;
