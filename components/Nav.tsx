6f8bd86b3cb69a8dd5e39eb24b78680761cb2978
Harsh Singh <hi.harsh@protonmail.ch>
2022-08-24 22:31:54 +0000

    Added aria-label for button elements

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { FC } from 'react'
 import { FiCommand } from 'react-icons/fi'
 import Link from 'next/link'
 import { useKmenu } from 'kmenu'
@@ -29,6 +29,8 @@ const Nav: FC<{ header?: string }> = ({ header }) => {
       <button
         className='bg-white shadow-2xl dark:shadow-none text-black dark:bg-gray-800 transition hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white w-12 h-12 rounded-lg flex justify-center text-xl items-center'
         onClick={() => setOpen(1)}
+        aria-label='Open Command Menu'
+        title='Open Command Menu'
       >
         <FiCommand size={25} strokeWidth={1.5} />
       </button>
