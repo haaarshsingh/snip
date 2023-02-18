872f30fc872da441e074d9eb2b02995e5f256ad2
Harsh Singh <hi.harsh@protonmail.ch>
2023-02-17 23:01:41 -0600

    Fix delay options

--- components/Options.tsx
+++ components/Options.tsx
@@ -195,7 +195,7 @@ const Option: FC<{
             initial={{ y: -10, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: 10, opacity: 0 }}
-            transition={{ delay: 0.2 }}>
+            transition={{ delay: 0.01 }}>
             {command.text}
           </motion.div>
         )}
