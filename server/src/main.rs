85c714a720880781fc56df3e0be192198a39184a
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-07-22 23:45:07 +0500

    initial backend commit

---
+++ server/src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("snip v2")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
