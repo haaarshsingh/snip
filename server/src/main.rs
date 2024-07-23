325241900519ded2d4a418494f2f1a1e171f1a31
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-07-24 04:17:29 +0500

    add get snip route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,36 @@
 mod snip;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
+use mongodb::{bson::doc, Client, Collection};
+use snip::Snip;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("snip v2")
+const DB_NAME: &str = "snipdb";
+const COLL_NAME: &str = "snips";
+
+#[get("/snips/get/{id}")]
+async fn get_snip(client: web::Data<Client>, id: web::Path<String>) -> HttpResponse {
+    let snip_id = id.into_inner();
+
+    let collection: Collection<Snip> = client.database(DB_NAME).collection(COLL_NAME);
+    match collection.find_one(doc! { "id": &snip_id }).await {
+        Ok(Some(user)) => HttpResponse::Ok().json(user),
+        Ok(None) => HttpResponse::NotFound().body(format!("no snip found with id: {snip_id}")),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(client.clone()))
+            .service(get_snip)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- server/src/snip.rs
+++ server/src/snip.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
-pub struct User {
+pub struct Snip {
     pub id: String,
     pub title: String,
     pub content: String,
