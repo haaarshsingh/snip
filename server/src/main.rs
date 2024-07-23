a97398421ff4500320147cbf3f0ff30eb25b7f9b
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-07-24 04:56:24 +0500

    refactor Snip to SnipObject to allow multiple snips

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,26 +3,28 @@ mod snip;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
 use mongodb::{bson::doc, Client, Collection};
-use snip::Snip;
+use snip::SnipObject;
 
 const DB_NAME: &str = "snipdb";
 const COLL_NAME: &str = "snips";
 
 #[get("/snips/get/{id}")]
 async fn get_snip(client: web::Data<Client>, id: web::Path<String>) -> HttpResponse {
-    let snip_id = id.into_inner();
+    let snip_object_id = id.into_inner();
 
-    let collection: Collection<Snip> = client.database(DB_NAME).collection(COLL_NAME);
-    match collection.find_one(doc! { "id": &snip_id }).await {
+    let collection: Collection<SnipObject> = client.database(DB_NAME).collection(COLL_NAME);
+    match collection.find_one(doc! { "id": &snip_object_id }).await {
         Ok(Some(user)) => HttpResponse::Ok().json(user),
-        Ok(None) => HttpResponse::NotFound().body(format!("no snip found with id: {snip_id}")),
+        Ok(None) => {
+            HttpResponse::NotFound().body(format!("no snip found with id: {snip_object_id}"))
+        }
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
 }
 
 #[post("/snips/create")]
-async fn create_snip(client: web::Data<Client>, new_snip: web::Json<Snip>) -> HttpResponse {
-    let collection: Collection<Snip> = client.database(DB_NAME).collection(COLL_NAME);
+async fn create_snip(client: web::Data<Client>, new_snip: web::Json<SnipObject>) -> HttpResponse {
+    let collection: Collection<SnipObject> = client.database(DB_NAME).collection(COLL_NAME);
     let result = collection.insert_one(new_snip.into_inner()).await;
 
     match result {
--- server/src/snip.rs
+++ server/src/snip.rs
@@ -1,8 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
-pub struct Snip {
+pub struct SnipObject {
     pub id: String,
+    pub snips: Vec<SnipObject>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize)]
+pub struct Snip {
     pub content: String,
     pub title: Option<String>,
     pub language: Option<String>,
