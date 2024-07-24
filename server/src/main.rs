18d8c7f00b909086a5afd203ca2904ebf52fe167
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2024-07-24 22:58:00 +0500

    refactor error handling to use json

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,12 @@
 mod snip;
 
+use std::env;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
 use mongodb::{bson::doc, Client, Collection};
 use nanoid::nanoid;
+use serde_json::json;
 use snip::SnipObject;
 
 const DB_NAME: &str = "snipdb";
@@ -16,11 +19,10 @@ async fn get_snip(client: web::Data<Client>, id: web::Path<String>) -> HttpRespo
     let collection: Collection<SnipObject> = client.database(DB_NAME).collection(COLL_NAME);
 
     match collection.find_one(doc! { "id": &snip_object_id }).await {
-        Ok(Some(user)) => HttpResponse::Ok().json(user),
-        Ok(None) => {
-            HttpResponse::NotFound().body(format!("no snip found with id: {snip_object_id}"))
-        }
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+        Ok(Some(snip)) => HttpResponse::Ok().json(snip),
+        Ok(None) => HttpResponse::NotFound()
+            .json(json!({ "error": format!("no snip found with id: {}", snip_object_id) })),
+        Err(err) => HttpResponse::InternalServerError().json(json!({ "error": err.to_string() })),
     }
 }
 
@@ -31,15 +33,15 @@ async fn create_snip(
 ) -> HttpResponse {
     let collection: Collection<SnipObject> = client.database(DB_NAME).collection(COLL_NAME);
 
-    if new_snip.id == None {
+    if new_snip.id.is_none() {
         new_snip.id = Some(nanoid!(3));
     }
 
     let result = collection.insert_one(new_snip.into_inner()).await;
 
     match result {
-        Ok(_) => HttpResponse::Ok().body("snip added successfully"),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+        Ok(_) => HttpResponse::Ok().json(json!({ "message": "snip added successfully" })),
+        Err(err) => HttpResponse::InternalServerError().json(json!({ "error": err.to_string() })),
     }
 }
 
@@ -47,7 +49,8 @@ async fn create_snip(
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let uri = env::var("MONGO_DB_URI")
+        .expect("MONGO_DB_URI environment variable not set! Check your .env file.");
 
     let client = Client::with_uri_str(uri).await.expect("failed to connect");
 
