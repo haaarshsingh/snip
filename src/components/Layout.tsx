ac36e3e691fb5244dd3e44cfda5ce1d02ff9ddac
Harsh Singh <hi.harsh@protonmail.ch>
2024-09-24 11:56:44 -0400

    configure monorepo

--- src/components/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, ReactNode } from 'react'
-
-export default (({ children }) => (
-  <div className='w-full flex justify-center'>
-    <div className='w-content'>{children}</div>
-  </div>
-)) as FC<{ children: ReactNode }>
