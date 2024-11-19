58ff397065bea0e8cc8d090a22af3bcc7ccfad97
Harsh Singh <hi.harsh@protonmail.ch>
2024-11-19 01:10:21 -0500

    add mapping

--- bot/src/commands/snip.rs
+++ bot/src/commands/snip.rs
@@ -1,6 +1,7 @@
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use std::collections::HashMap;
 
 #[command]
 async fn snip(ctx: &Context, msg: &Message) -> CommandResult {
@@ -30,16 +31,14 @@ fn extract_codeblocks(message: &str) -> Vec<CodeBlock> {
     for line in message.lines() {
         if line.starts_with("```") {
             if in_codeblock {
-                // End of codeblock
                 codeblocks.push(CodeBlock {
-                    language: language.clone(),
+                    language: extension_to_language(language.clone().as_str()),
                     content: content.clone(),
                 });
                 in_codeblock = false;
                 language.clear();
                 content.clear();
             } else {
-                // Start of codeblock
                 in_codeblock = true;
                 language = line.trim_start_matches("```").to_string();
             }
@@ -50,6 +49,125 @@ fn extract_codeblocks(message: &str) -> Vec<CodeBlock> {
     }
 
     codeblocks
+}
+
+fn extension_to_language(extension: &str) -> String {
+    let languages: Vec<(&str, &str)> = vec![
+        ("", "Autodetect"),
+        ("apl", "APL"),
+        ("asc", "ASCII Armor"),
+        ("asterisk", "Asterisk"),
+        ("bf", "Brainfuck"),
+        ("c", "C"),
+        ("cpp", "C++"),
+        ("cs", "C#"),
+        ("cql", "Cassandra"),
+        ("ceylon", "Ceylon"),
+        ("clj", "Clojure"),
+        ("cmake", "CMake"),
+        ("cbl", "Cobol"),
+        ("coffee", "CoffeeScript"),
+        ("lisp", "CommonLisp"),
+        ("cr", "Crystal"),
+        ("css", "CSS"),
+        ("pyx", "Cython"),
+        ("d", "D"),
+        ("dart", "Dart"),
+        ("diff", "diff"),
+        ("Dockerfile", "Dockerfile"),
+        ("dtd", "DTD"),
+        ("dylan", "Dylan"),
+        ("ebnf", "EBNF"),
+        ("ecl", "ECL"),
+        ("e", "Eiffel"),
+        ("elm", "Elm"),
+        ("erl", "Erlang"),
+        ("esper", "Esper"),
+        ("fs", "F#"),
+        ("fcl", "FCL"),
+        ("f90", "Fortran"),
+        ("forth", "Forth"),
+        ("s", "Gas"),
+        ("arm", "Gas ARM"),
+        ("feature", "Gherkin"),
+        ("go", "Go"),
+        ("gql", "GraphQL"),
+        ("groovy", "Groovy"),
+        ("hs", "Haskell"),
+        ("hx", "Haxe"),
+        ("hql", "Hive"),
+        ("html", "HTML"),
+        ("http", "HTTP"),
+        ("idl", "IDL"),
+        ("java", "Java"),
+        ("js", "JavaScript"),
+        ("j2", "Jinja2"),
+        ("json", "JSON"),
+        ("jsonld", "JSON-LD"),
+        ("jsx", "JSX"),
+        ("jl", "Julia"),
+        ("kt", "Kotlin"),
+        ("less", "LESS"),
+        ("ls", "LiveScript"),
+        ("lua", "Lua"),
+        ("md", "Markdown"),
+        ("sql", "MariaDB SQL"),
+        ("nb", "Mathematica"),
+        ("mbox", "mbox"),
+        ("mir", "MIR"),
+        ("mscgen", "MscGen"),
+        ("msgenny", "MsGenny"),
+        ("mps", "Mumps"),
+        ("nginx", "nginx"),
+        ("nc", "NesC"),
+        ("nt", "NTriples"),
+        ("mm", "Objective C++"),
+        ("m", "Objective-C"),
+        ("ml", "oCaml"),
+        ("pas", "Pascal"),
+        ("pl", "Perl"),
+        ("php", "PHP"),
+        ("pig", "Pig"),
+        ("pls", "PLSQL"),
+        ("ps1", "PowerShell"),
+        ("proto", "Protobuf"),
+        ("py", "Python"),
+        ("q", "Q"),
+        ("r", "R"),
+        ("rpm", "RPM"),
+        ("rb", "Ruby"),
+        ("rs", "Rust"),
+        ("sas", "SAS"),
+        ("sass", "Sass"),
+        ("scala", "Scala"),
+        ("scm", "Scheme"),
+        ("scss", "SCSS"),
+        ("sh", "Shell"),
+        ("st", "Smalltalk"),
+        ("sml", "SML"),
+        ("solr", "Solr"),
+        ("styl", "Stylus"),
+        ("swift", "Swift"),
+        ("tcl", "Tcl"),
+        ("tsx", "TSX"),
+        ("ttcn", "TTCN"),
+        ("ttl", "Turtle"),
+        ("ts", "TypeScript"),
+        ("vb", "Visual Basic"),
+        ("vbs", "VBScript"),
+        ("vhdl", "VHDL"),
+        ("xml", "XML"),
+        ("xq", "XQuery"),
+        ("yaml", "YAML"),
+        ("z80", "Z80"),
+    ];
+
+    let language_map: HashMap<&str, &str> = languages.into_iter().collect();
+
+    language_map
+        .get(extension)
+        .unwrap_or(&"Autodetect")
+        .to_string()
 }
 
 #[derive(Debug)]
